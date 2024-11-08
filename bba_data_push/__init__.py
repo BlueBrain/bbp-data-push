9ed7ec445f37179951dbe6d3f7dd0dce4dc7a7dd
Leonardo Cristella <leonardo.cristella@epfl.ch>
2024-11-08 14:51:00 +0100

    Use correct package name in __init__.py

--- bba_data_push/__init__.py
+++ bba_data_push/__init__.py
@@ -2,10 +2,9 @@
 Version corresponding to the git version tag
 """
 from pkg_resources import get_distribution, DistributionNotFound
-from bba_data_push import __name__
 
 try:
-    __version__ = get_distribution(__name__).version
+    __version__ = get_distribution('blue_brain_data_push').version
 except DistributionNotFound:
     # package is not installed
     pass
